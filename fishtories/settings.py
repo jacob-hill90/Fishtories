d393b3837a30eea196791bbf5fafa4d6ba330d68
Jake <hilljake93@gmail.com>
2024-02-22 12:15:31 -0500

    made a mistake

--- fishtories/settings.py
+++ fishtories/settings.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-AWS_ACCESS_KEY_ID = 'AKIA4RD6W6S7FEGBYFUH'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'fishtories'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'us-east-1'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME") 
+AWS_S3_SIGNATURE_NAME = os.environ.get("AWS_S3_SIGNATURE_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
@@ -88,10 +88,10 @@ WSGI_APPLICATION = 'fishtories.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "fishdb",
-        "USER": "jake",
-        "PASSWORD": "Changeme123!!",
-        "HOST": "database-1.cz40ow8eibnc.us-east-1.rds.amazonaws.com",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PW"),
+        "HOST": os.environ.get("DB_HOST"),
         "PORT": "5432",
     }
 }
