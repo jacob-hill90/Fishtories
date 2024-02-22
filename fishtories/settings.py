b28bfdac5d40e09a557a9efc39a46e8c95ac7c9e
Jake <hilljake93@gmail.com>
2024-02-22 10:55:21 -0500

    Working on aws stuff

--- fishtories/settings.py
+++ fishtories/settings.py
@@ -64,14 +64,25 @@ WSGI_APPLICATION = 'fishtories.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+#         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+#         "USER": os.environ.get("SQL_USER", "user"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+#         "HOST": os.environ.get("SQL_HOST", "localhost"),
+#         "PORT": os.environ.get("SQL_PORT", "5432"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "fishdb",
+        "USER": "jake",
+        "PASSWORD": "Changeme123!!",
+        "HOST": "database-1.cz40ow8eibnc.us-east-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
 
